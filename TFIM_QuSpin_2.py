8f68c9573d1583bb790c2b3d20df5ebbac23e6e2
Rok Mlinar Vahtar <rokmlinar.p@gmial.com>
2023-08-01 14:30:45 +0200

    Komentarji

--- TFIM_QuSpin_2.py
+++ TFIM_QuSpin_2.py
@@ -2,7 +2,7 @@ from quspin.operators import hamiltonian # Hamiltonians and operators
 from quspin.basis import spin_basis_1d# Hilbert space spin basis
 import numpy as np # generic math functions
 import matplotlib.pyplot as plt # plotting library
-import TFIM_QuSpin as OneChain
+import TFIM_QuSpin as OneChain #TFIM model z eno verigo za primerjavo
 
 def bin_array(num:int, m:int) -> list:
     """Convert a positive integer num into an m-bit bit vector"""
