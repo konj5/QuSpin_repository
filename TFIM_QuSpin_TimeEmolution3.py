acba750f4c0842f4d5e498006e8977220b239a1c
Rok Mlinar <rokmlinar.p@gmail.com>
2023-08-26 16:13:04 +0200

    Updated naming scheme

---
+++ TFIM_QuSpin_TimeEmolution3.py
@@ -0,0 +1,5 @@
+from quspin.operators import hamiltonian # Hamiltonians and operators
+from quspin.basis import spin_basis_1d# Hilbert space spin basis
+import numpy as np # generic math functions
+import matplotlib.pyplot as plt # plotting library
+import TFIM_QuSpin_TimeEvolution2 as evo # time evolution for our specific problem
